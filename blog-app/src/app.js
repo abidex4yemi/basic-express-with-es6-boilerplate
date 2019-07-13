e6beb56567a617e2f8305a92ca12a61cd826982f
abidex4yemi <abidex4yemi@gmail.com>
2019-07-13 22:45:05 +0100

    Convert  app.js file to use es6 syntax

--- blog-app/src/app.js
+++ blog-app/src/app.js
@@ -1,20 +1,18 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+import express from 'express';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
 
-var indexRouter = require('../routes/index');
-var usersRouter = require('../routes/users');
+import { homeRouter } from './routes/index';
 
-var app = express();
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', homeRouter);
 
-module.exports = app;
+export default app;
